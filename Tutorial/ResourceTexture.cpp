13ac2d6d23e151f88237bfd7b84f34b821cad67d
Skrobson <skrobolbartlomiej@gmail.com>
2016-06-19 08:49:07 +0200

    Update ResourceTexture.cpp
    use_count==0 oznacza wskaźnik pusty, mogło by być use_count()==1 ale unique() jest szybsze

--- Tutorial/ResourceTexture.cpp
+++ Tutorial/ResourceTexture.cpp
@@ -60,7 +60,7 @@ bool ResourceTexture::LoadResource(std::string dir, SDL_Renderer * renderer)
 
 bool ResourceTexture::Destroy()
 {
-	if (initialized && texture.use_count() == 0)
+	if (initialized && texture.unique())
 	{
 		SDL_DestroyTexture(texture.get());
 		texture.reset();
@@ -68,7 +68,7 @@ bool ResourceTexture::Destroy()
 	}
 	else
 	{
-		DEBUG_LOG("Trying to destroy texture with more than zero references");
+		DEBUG_LOG("Trying to destroy texture with more than one references");
 		return false;
 	}
 }
